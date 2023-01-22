9c6dc8e3d0de5acb680589478c7d0a06dd556778
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-01-21 22:50:57 -0300

    Aula 03

---
+++ web/src/components/NewHabitForm.tsx
@@ -0,0 +1,21 @@
+import { Check } from 'phosphor-react'
+
+export function NewHabitForm() {
+  return (
+    <form>
+      <label htmlFor="title">Qual e seu comprometimento</label>
+      <input
+        type="text"
+        id="title"
+        placeholder="ex.: Exercícios, dormir bem, etc"
+        autoFocus
+      />
+
+      <label htmlFor="">Qual a recorrência</label>
+      <button type="submit">
+        <Check size={20} weight="bold" />
+        Confirmar
+      </button>
+    </form>
+  )
+}
