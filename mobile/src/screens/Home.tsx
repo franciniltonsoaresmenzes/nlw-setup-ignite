46cede24697de1df2029cc57105d8fa0dc139d28
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-01-22 20:35:58 -0300

    Aula 03 - finalizada

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { View, Text, ScrollView } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
@@ -9,6 +10,7 @@ const mininumSummaryDatesSize = 18 * 5
 const amountOfDaysFill = mininumSummaryDatesSize - datesFromYearsStart.length
 
 export function Home() {
+  const { navigate } = useNavigation()
   return (
     <View className="flex-1 bg-background px-8 pt-16" >
       <Header />
@@ -29,7 +31,10 @@ export function Home() {
         >
           <View className="flex-row flex-wrap" >
             {datesFromYearsStart.map(date => (
-              <HabitDay key={date.toISOString()} />
+              <HabitDay 
+                key={date.toISOString()} 
+                onPress={() => navigate('habit', { date: date.toISOString() })}
+              />
             ))}
 
           {amountOfDaysFill > 0 && Array.from({ length: amountOfDaysFill }).map((_, i) => (
