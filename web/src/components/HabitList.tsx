799016514b6554771611095bc3db7f37b1a2951b
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-01-23 19:44:47 -0300

    Preview

--- web/src/components/HabitList.tsx
+++ web/src/components/HabitList.tsx
@@ -63,7 +63,7 @@ export function HabitList({ date, onCompletedChanged }: HabitListProps) {
           disabled={isDateInPast}
           className="flex items-center gap-3 group"
         >
-          <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+          <div className="h-8 w-8 transition-all rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
             <Checkbox.Indicator>
               <Check size={20} className="text-white" />
             </Checkbox.Indicator>
